604fcf0f0bffeba0974f999136cc92dbf5209efa
nishidasha <1111>
2020-06-17 08:47:18 +0800

    6.16 d182

--- src/main/java/com/example/springbootmanagerums/controller/TestController.java
+++ src/main/java/com/example/springbootmanagerums/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.springbootmanagerums.controller;
 
+import com.example.springbootmanagerums.entity.Course;
 import com.example.springbootmanagerums.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,17 @@ import java.util.Map;
 
 @Controller
 public class TestController {
+
+    @GetMapping("/course")
+    public String Classs(Map map){
+        List list = new ArrayList();
+        list.add(new Course("中华文化传统","","商务礼仪","","Juery"));
+        list.add(new Course("","Spark","数据挖掘","数据挖掘","Juery"));
+        list.add(new Course("JavaWeb","Spark","数据库技术","",""));
+        list.add(new Course("JavaWeb","","数据库技术","",""));
+        map.put("cou",list);
+        return "course";
+    }
 
     @GetMapping(path = "/")
     public String hello(){
---
+++ src/main/java/com/example/springbootmanagerums/entity/Course.java
@@ -0,0 +1,14 @@
+package com.example.springbootmanagerums.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Course {
+    private String c1;
+    private String c2;
+    private String c3;
+    private String c4;
+    private String c5;
+}
