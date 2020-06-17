d47b25c343491bf4750483ffe78309cc5471e17f
nishidasha <1111>
2020-06-17 11:04:08 +0800

    6.16 d182

--- src/main/java/com/example/springbootmanagerums/controller/TestController.java
+++ src/main/java/com/example/springbootmanagerums/controller/TestController.java
@@ -1,27 +1,10 @@
 package com.example.springbootmanagerums.controller;
 
-import com.example.springbootmanagerums.entity.Course;
-import com.example.springbootmanagerums.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 @Controller
 public class TestController {
-
-    @GetMapping("/course")
-    public String Classs(Map map){
-        List list = new ArrayList();
-        list.add(new Course("中华文化传统","","商务礼仪","","Juery"));
-        list.add(new Course("","Spark","数据挖掘","数据挖掘","Juery"));
-        list.add(new Course("JavaWeb","Spark","数据库技术","",""));
-        list.add(new Course("JavaWeb","","数据库技术","",""));
-        map.put("cou",list);
-        return "course";
-    }
 
     @GetMapping(path = "/")
     public String hello(){
---
+++ src/main/java/com/example/springbootmanagerums/entity/Manager.java
@@ -0,0 +1,19 @@
+package com.example.springbootmanagerums.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Manager {
+
+    private int mg_id;
+    private String mg_name;
+    private char mg_pwd;
+    private int mg_time;
+    private String mg_mobile;
+    private String mg_email;
+    private int mg_state;
+}
